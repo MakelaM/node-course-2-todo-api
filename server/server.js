45d78cb08873163a2fabd42736dd43b353398625
Mika <mika.m.makela@gmail.com>
2017-11-30 15:26:39 +0200

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -32,10 +32,6 @@ app.get('/todos', (req, res) => {
     })
 });
 
-app.listen(port, () => {
-    console.log(`Started on port ${port}`);
-});
-
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
 
@@ -57,6 +53,27 @@ app.get('/todos/:id', (req, res) => {
         res.status(400).send();
     });
 
+});
+
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    };   
+    
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send(); 
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
 });
 
 module.exports = {app};
