82dee8548ccffa7e3435a3b348fa147b29ca6464
Mika <mika.m.makela@gmail.com>
2017-12-01 11:52:59 +0200

    Nothing special

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,5 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI ,{ useMongoClient: true })
-// mongoose.connect('mongodb://MakelaM:Tietokanta1#@ds151014.mlab.com:51014/todoappdb');
-
 
 module.exports = {mongoose};
